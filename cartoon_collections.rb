3cbc6f1ab8ab0c7ad7347b1ab198726d60a1c7ba
brenden williams <brendenw7@gmail.com>
2019-03-29 16:19:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves_array)# code an argument here
   # Your code here
+  counter = 0
+  dwarves_array.size.times {puts "#{counter+=1}.#{dwarves_array.unshift}"}
+  
+  #dwarves_array.each_with_index 
 end
 
 def summon_captain_planet# code an argument here
